7503b11a8d515e7d2d37a2a49cd672e7bbc14bbe
IsoJulien <julien.moura@isogeo.fr>
2016-11-24 23:03:00 +0100

    update make batch file

--- make.bat
+++ make.bat
@@ -4,15 +4,13 @@ pylupdate4 -noobsolete -verbose isogeo_search_engine.pro
 Echo "Compilating QT ui to Python classes"
 pyuic4 -x "ui\isogeo_dockwidget_base.ui" -o "ui\ui_isogeo.py"
 pyuic4 -x "ui\auth\ui_authentication.ui" -o "ui\auth\ui_authentication.py"
-pyuic4 -x "ui\mddetails\isogeo_md_details.ui" -o "ui\mddetails\ui_isogeo_md_details.py"
-pyuic4 -x "ui\name\ask_research_name.ui" -o "ui\name\ui_ask_research_name.py"
-pyuic4 -x "ui\rename\ask_new_name.ui" -o "ui\rename\ui_ask_new_name.py"
+pyuic4 -x "ui\credits\ui_credits.ui" -o "ui\credits\ui_credits.py"
+pyuic4 -x "ui\mddetails\ui_md_details.ui" -o "ui\mddetails\ui_md_details.py"
+pyuic4 -x "ui\quicksearch\ui_quicksearch_rename.ui" -o "ui\quicksearch\ui_quicksearch_rename.py"
+pyuic4 -x "ui\quicksearch\ui_quicksearch_new.ui" -o "ui\quicksearch\ui_quicksearch_new.py"
 
 Echo "Updating Qt resources files (images)"
 pyrcc4 -o resources.py resources.qrc
 
 Echo "End of compilation."
 Echo "Remind that you have to remove 'import resources_rc' at the end of py files generated from ui."
-
-
-rem @sed -re 's/import resources_rc/from QuickOSM import resources_rc/g' ui/osm_file_temp.py > ui/osm_file.py
