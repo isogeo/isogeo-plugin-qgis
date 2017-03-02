4f5f2fccc56e7fb39a9c78f40c0e1aa8be269f2d
IsoJulien <julien.moura@isogeo.fr>
2017-03-02 13:48:37 +0100

    more complete script #93

--- test/test_wms.py
+++ test/test_wms.py
@@ -2,6 +2,7 @@
 # ######## Imports #################
 # ################################
 
+from urllib import unquote, urlencode
 from sys import exit
 
 try:
@@ -21,6 +22,21 @@ except ImportError as e:
     print("Depencencies - HTTPError not within owslib."
                  " Trying to get it from urllib2 directly.")
     from urllib2 import HTTPError
+
+# ################################
+# ######## Variables  ###############
+# ################################
+
+current_crs = str(iface.mapCanvas()
+                                 .mapRenderer()
+                                 .destinationCrs()
+                                 .authid())
+
+qgis_wms_formats = ('image/png', 'image/png8',
+                              'image/jpeg',
+                              'image/svg',
+                              'image/gif',
+                              'image/geotiff', 'image/tiff')
 
 # ################################
 # ######## Script  #################
@@ -52,8 +68,71 @@ else:
 
 # get a layer
 print("Available layers: ", list(wms.contents))
-layer_name = wms[list(wms.contents)[0]]
+layer_name = list(wms.contents)[0]
+wms_lyr = wms[layer_name]
+layer_title = wms_lyr.title
 
 # is queryable
-assert(layer_name.queryable)
+assert(wms_lyr.queryable)
 
+# SRS
+print("Available SRS: ", wms_lyr.crsOptions)
+if current_crs in wms_lyr.crsOptions:
+    print("It's a SRS match! With map canvas: " + current_crs)
+    srs = current_crs
+elif "EPSG:4326" in wms_lyr.crsOptions:
+    print("It's a SRS match! With standard WGS 84 (EPSG:4326)")
+    srs = "EPSG:4326"
+else:
+    print("Searched SRS not available within service CRS.")
+    srs = ""
+
+# Format definition
+wms_lyr_formats = wms.getOperationByName('GetMap').formatOptions
+formats_image = [f.split(" ", 1)[0] for f in wms_lyr_formats
+                          if f in qgis_wms_formats]
+if "image/png" in formats_image:
+    layer_format = "image/png"
+elif "image/jpeg" in formats_image:
+    layer_format = "image/jpeg"
+else:
+    layer_format = formats_image[0]
+
+# Style definition
+print("Available styles: ", wms_lyr.styles)
+lyr_style = wms_lyr.styles.keys()[0]
+
+# GetMap URL
+wms_lyr_url = wms.getOperationByName('GetMap').methods
+wms_lyr_url = wms_lyr_url[0].get("url")
+if wms_lyr_url[-1] == "&":
+    wms_lyr_url = wms_lyr_url[:-1]
+else:
+    pass
+
+# URL construction
+wms_url_params = {"service": "WMS",
+                           #"version": "1.3.0",
+                           "request": "GetMap",
+                           "layers": layer_name,
+                           "crs": srs,
+                           "format": layer_format,
+                           "styles": lyr_style,
+                           "url": wms_lyr_url,
+                            }
+wms_url_final = unquote(urlencode(wms_url_params))
+print(wms_url_final)
+
+# let's try to add it to the map canvas
+qgis_wms_lyr = QgsRasterLayer(wms_url_final, "Auto - " + layer_title, 'wms')
+if qgis_wms_lyr.isValid():
+    QgsMapLayerRegistry.instance().addMapLayer(qgis_wms_lyr)
+else:
+    print(qgis_wms_lyr.error().message())
+
+manual_lyr = "layers=prescription_pct&crs=EPSG:4326&version=1.1.0&url=http://geobretagne.fr/geoserver/lorientagglo/wms?SERVICE=WMS&service=WMS&format=image/png&styles=&request=GetMap"
+qgis_wms_lyr = QgsRasterLayer(manual_lyr, "Manual - " + layer_title, 'wms')
+if qgis_wms_lyr.isValid():
+    QgsMapLayerRegistry.instance().addMapLayer(qgis_wms_lyr)
+else:
+    print(qgis_wms_lyr.error().message())
