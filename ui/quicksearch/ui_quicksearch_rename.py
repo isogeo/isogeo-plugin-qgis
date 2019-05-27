140de57a1f0479859700a958948fa662cf02b813
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2019-05-27 13:50:01 +0200

    compiling ui with Qt5
    close #171

--- ui/quicksearch/ui_quicksearch_rename.py
+++ ui/quicksearch/ui_quicksearch_rename.py
@@ -2,44 +2,29 @@
 
 # Form implementation generated from reading ui file 'ui\quicksearch\ui_quicksearch_rename.ui'
 #
-# Created: Thu May 31 19:44:28 2018
-#      by: PyQt4 UI code generator 4.10.2
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_dlg_quicksearch_rename(object):
     def setupUi(self, dlg_quicksearch_rename):
-        dlg_quicksearch_rename.setObjectName(_fromUtf8("dlg_quicksearch_rename"))
+        dlg_quicksearch_rename.setObjectName("dlg_quicksearch_rename")
         dlg_quicksearch_rename.resize(498, 100)
         dlg_quicksearch_rename.setMinimumSize(QtCore.QSize(340, 100))
         dlg_quicksearch_rename.setMaximumSize(QtCore.QSize(700, 116))
         dlg_quicksearch_rename.setFocusPolicy(QtCore.Qt.StrongFocus)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/plugins/Isogeo/resources/search/bolt.svg")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/plugins/Isogeo/resources/search/bolt.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         dlg_quicksearch_rename.setWindowIcon(icon)
         dlg_quicksearch_rename.setWindowOpacity(0.9)
         dlg_quicksearch_rename.setAutoFillBackground(True)
         dlg_quicksearch_rename.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         dlg_quicksearch_rename.setModal(True)
-        self.gridLayout = QtGui.QGridLayout(dlg_quicksearch_rename)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.lbl_title = QtGui.QLabel(dlg_quicksearch_rename)
+        self.gridLayout = QtWidgets.QGridLayout(dlg_quicksearch_rename)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lbl_title = QtWidgets.QLabel(dlg_quicksearch_rename)
         font = QtGui.QFont()
         font.setPointSize(8)
         font.setBold(True)
@@ -48,33 +33,34 @@ class Ui_dlg_quicksearch_rename(object):
         self.lbl_title.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
         self.lbl_title.setAutoFillBackground(True)
         self.lbl_title.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
-        self.lbl_title.setObjectName(_fromUtf8("lbl_title"))
+        self.lbl_title.setObjectName("lbl_title")
         self.gridLayout.addWidget(self.lbl_title, 0, 1, 1, 1)
-        self.txt_quicksearch_rename = QtGui.QLineEdit(dlg_quicksearch_rename)
+        self.txt_quicksearch_rename = QtWidgets.QLineEdit(dlg_quicksearch_rename)
         self.txt_quicksearch_rename.setMinimumSize(QtCore.QSize(50, 25))
-        self.txt_quicksearch_rename.setObjectName(_fromUtf8("txt_quicksearch_rename"))
+        self.txt_quicksearch_rename.setObjectName("txt_quicksearch_rename")
         self.gridLayout.addWidget(self.txt_quicksearch_rename, 0, 2, 1, 1)
-        self.btn_save_cancel = QtGui.QDialogButtonBox(dlg_quicksearch_rename)
+        self.btn_save_cancel = QtWidgets.QDialogButtonBox(dlg_quicksearch_rename)
         self.btn_save_cancel.setOrientation(QtCore.Qt.Horizontal)
-        self.btn_save_cancel.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Save)
+        self.btn_save_cancel.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Save)
         self.btn_save_cancel.setCenterButtons(True)
-        self.btn_save_cancel.setObjectName(_fromUtf8("btn_save_cancel"))
+        self.btn_save_cancel.setObjectName("btn_save_cancel")
         self.gridLayout.addWidget(self.btn_save_cancel, 2, 0, 1, 3)
 
         self.retranslateUi(dlg_quicksearch_rename)
-        QtCore.QObject.connect(self.btn_save_cancel, QtCore.SIGNAL(_fromUtf8("accepted()")), dlg_quicksearch_rename.accept)
-        QtCore.QObject.connect(self.btn_save_cancel, QtCore.SIGNAL(_fromUtf8("rejected()")), dlg_quicksearch_rename.reject)
+        self.btn_save_cancel.accepted.connect(dlg_quicksearch_rename.accept)
+        self.btn_save_cancel.rejected.connect(dlg_quicksearch_rename.reject)
         QtCore.QMetaObject.connectSlotsByName(dlg_quicksearch_rename)
 
     def retranslateUi(self, dlg_quicksearch_rename):
-        dlg_quicksearch_rename.setWindowTitle(_translate("dlg_quicksearch_rename", "Isogeo - Rename quicksearch", None))
-        self.lbl_title.setText(_translate("dlg_quicksearch_rename", "Quicksearch new name:", None))
+        _translate = QtCore.QCoreApplication.translate
+        dlg_quicksearch_rename.setWindowTitle(_translate("dlg_quicksearch_rename", "Isogeo - Rename quicksearch"))
+        self.lbl_title.setText(_translate("dlg_quicksearch_rename", "Quicksearch new name:"))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    dlg_quicksearch_rename = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    dlg_quicksearch_rename = QtWidgets.QDialog()
     ui = Ui_dlg_quicksearch_rename()
     ui.setupUi(dlg_quicksearch_rename)
     dlg_quicksearch_rename.show()
