8af616a22aaabb403ba0bd2c370d4de303c6e703
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2019-08-22 18:19:46 +0200

    creating modules/api/shares.py module
    #177 #215

---
+++ modules/api/shares.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Standard library
+import logging
+
+# PyQGIS
+from qgis.core import QgsNetworkAccessManager
+
+# PyQT
+from qgis.PyQt.QtCore import pyqtSignal, QThread, QObject, pyqtSlot
+
+# Plugin modules
+from ..tools import IsogeoPlgTools
+
+# ############################################################################
+# ########## Globals ###############
+# ##################################
+
+logger = logging.getLogger("IsogeoQgisPlugin")
+plg_tools = IsogeoPlgTools()
+# ############################################################################
+# ########## Classes ###############
+# ##################################
+
+
+class SharesParser(QObject):
+    """Build the string informing the user about the shares feeding his plugin 
+    from the Isogeo API's response to a share request.
+    """
+    shares_ready = pyqtSignal(str)
+
+    def __init__(self):
+        QObject.__init__(self)
+        self.tr = object
+
+    @pyqtSlot(list)
+    def send_share_info(self, result: list):
+        """Slot connected to ApiRequester.shares_sig signal emitted when a response 
+        to a share request has been received from the Isogeo's API, validated and parsed.
+        'result' parameter correspond to the content of shares request's reply passed 
+        by the ApiRequester.handle_reply method (see modules/api/request.py). 
+        Once the string building from 'result' is done, the shares_ready signal is emitted 
+        passing this string to a connected slot : Isogeo.write_shares_info (see isogeo.py).
+        
+        :param list result: list of shares feeding the application
+        """
+        logger.debug("Application properties provided by the Isogeo API.")
+        content = result
+
+        text = u"<html>"  # opening html content
+        # Isogeo application authenticated in the plugin
+        app = content[0].get("applications")[0]
+        text += self.tr(u"<p>This plugin is authenticated as "
+                        u"<a href='{}'>{}</a> and ").format(app.get("url", "https://isogeo.gitbooks.io/app-plugin-qgis/content"),
+                            app.get("name", "Isogeo plugin for QGIS"))
+        # shares feeding the application
+        if len(content) == 1:
+            text += self.tr(u" powered by 1 share:</p></br>")
+        else:
+            text += self.tr(u" powered by {} shares:</p></br>").format(len(content))
+        # shares details
+        for share in content:
+            # share variables
+            creator_name = share.get("_creator").get("contact").get("name")
+            creator_email = share.get("_creator").get("contact").get("email")
+            creator_id = share.get("_creator").get("_tag")[6:]
+            share_url = "https://app.isogeo.com/groups/{}/admin/shares/{}".format(creator_id, share.get("_id"))
+            # formatting text
+            text += u"<p><a href='{}'><b>{}</b></a></p>".format(share_url,
+                            share.get("name"))
+            text += self.tr(u"<p>Updated: {}</p>").format(plg_tools.handle_date(share.get("_modified")))
+            text += self.tr(u"<p>Contact: {} - {}</p>").format(creator_name, creator_email)
+            text += u"<p><hr></p>"
+        text += u"</html>"
+        self.shares_ready.emit(text)
+
+# #############################################################################
+# ##### Stand alone program ########
+# ##################################
+if __name__ == '__main__':
+    """Standalone execution."""
