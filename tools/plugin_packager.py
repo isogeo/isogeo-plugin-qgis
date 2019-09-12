bac88734d735e52c6bc9ce552af206fb9f93febd
Julien <Guts@users.noreply.github.com>
2019-09-12 18:04:23 +0200

    CI - Add packaging step (#248)
    * publish code after lint job
    
    * compelting qt5 job
    
    * fix artifact name
    
    * fix job name
    
    * improve artifacts
    
    * add ui files compilation
    
    * rename compiled resources.py into resources_rc.py #245
    
    * fix packager after renaming resources compiled files
    
    * add every compiled translation file (*.qm) instead of specific listing
    
    * minor rename
    
    * move i18n
    
    * package ui dependecies (*.ui and dlg_*.py) in fewer lines
    
    * add packaging step
    
    * typo
    
    * fix artifact path
    
    * fix again artifact path
    
    * use the absolute path to the zip
    
    * do not lint tools and ui python files
    
    * use a global var for the final ZIP path
    
    * clean up
    
    * zip resources
    
    * os.path to pathlib.Path
    
    * os.path to pathlib.Path
    
    * remove unused script
    
    * achieve pacakger refactoring

--- tools/plugin_packager.py
+++ tools/plugin_packager.py
@@ -9,9 +9,9 @@
 
         1. Launch OSGeo4W Shell inside th Isogeo QGIS Plugin repository
         2. Run:
-            ```bash
+            
             python tools\plugin_packager.py
-            ```
+            
 
     See: https://docs.qgis.org/testing/en/docs/pyqgis_developer_cookbook/releasing.html
 
@@ -27,7 +27,7 @@
 
 # Standard library
 from configparser import ConfigParser
-from os import listdir, makedirs, path, remove, walk
+from os import listdir, path
 import json
 import xml.etree.ElementTree as ET
 import zipfile
@@ -38,6 +38,9 @@ from pathlib import Path
 # ########## Globals ###############
 # ##################################
 
+# TYPES
+RESOURCES_TYPES = ("**/*.svg", "**/*.png")
+
 # Paths
 DIR_PLUGIN_ROOT = Path(__file__).parent.parent
 DIR_OUTPUT = DIR_PLUGIN_ROOT.resolve() / "build/latest"
@@ -47,6 +50,8 @@ BASE_DIR_ABS = DIR_PLUGIN_ROOT.resolve()
 
 PLG_DIRNAME = "isogeo_search_engine"
 PLG_METADATA_FILE = DIR_PLUGIN_ROOT.resolve() / "metadata.txt"
+PLG_FINAL_ZIP_PATH = str(DIR_OUTPUT / PLG_DIRNAME) + ".zip"
+
 
 # #############################################################################
 # ########## Functions##############
@@ -57,16 +62,16 @@ def fix_ui_files(ui_folder: Path = "./ui"):
     """
     ui_folder = Path(ui_folder)
     for ui_file in ui_folder.glob("**/*.ui"):
-        with ui_file.open("r") as ui_file:
-            ui_xml = ET.parse(ui_file)
+        with ui_file.open("r") as f:
+            ui_xml = ET.parse(f)
             root = ui_xml.getroot()
             for rsrc in root.iter("resources"):
                 if len(rsrc) > 0:
-                    print("WARNING - resources tag spotted in: {}".format(f))
+                    print("WARNING - resources tag spotted in: {}".format(ui_file))
                     # AUTO REMOVE ##
                     root.remove(rsrc)
                     ui_xml.write(
-                        path.join(dirpath, f),
+                        ui_file.resolve(),
                         encoding="utf-8",
                         xml_declaration='version="1.0"',
                         method="xml",
@@ -79,7 +84,7 @@ def fix_ui_files(ui_folder: Path = "./ui"):
                 if header.text != "qgis.gui":
                     header.text = header.text.replace(header.text, "qgis.gui")
                     ui_xml.write(
-                        path.join(dirpath, f),
+                        ui_file.resolve(),
                         encoding="utf-8",
                         xml_declaration='version="1.0"',
                         method="xml",
@@ -120,19 +125,19 @@ remove_files()  # remove Python compiled files
 # get plugin metadata
 version = plugin_metadata()
 print(
-    "Packaging the version {} of the Isogeo plugin for QGIS version {} to {})".format(
-        *version
+    "Packaging the version {} of the Isogeo plugin for QGIS version {} to {}) to: {}".format(
+        *version, PLG_FINAL_ZIP_PATH
     )
 )
 
 # ensure UI files are good
 fix_ui_files()
 
-# ------------ Led Zipping -------------------------------------------
-RELEASE_ZIP = zipfile.ZipFile(
-    path.join(DIR_OUTPUT.resolve(), PLG_DIRNAME + ".zip"), "w"
-)
+# -- LED ZIPPING ----------------------------------------------------------
+RELEASE_ZIP = zipfile.ZipFile(PLG_FINAL_ZIP_PATH, "w")
 
+
+# -- REQUIRED EMPTIES FOLDERS -------------------------------------------------------
 # AUTH folder
 auth_folder = zipfile.ZipInfo(path.join(PLG_DIRNAME, "_auth/"))
 RELEASE_ZIP.writestr(auth_folder, "")
@@ -141,167 +146,55 @@ RELEASE_ZIP.writestr(auth_folder, "")
 log_folder = zipfile.ZipInfo(path.join(PLG_DIRNAME, "_logs/"))
 RELEASE_ZIP.writestr(log_folder, "")
 
-
 # USER folder
 user_folder = zipfile.ZipInfo(path.join(PLG_DIRNAME, "_user/"))
 RELEASE_ZIP.writestr(user_folder, "")
 
-# QGIS Plugin requirements
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "LICENSE"), "{}/{}".format(PLG_DIRNAME, "LICENSE")
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "metadata.txt"), "{}/{}".format(PLG_DIRNAME, "metadata.txt")
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "README.md"), "{}/{}".format(PLG_DIRNAME, "README")
-)
+# -- QGIS PLUGIN REQUIRED FILES -------------------------------------------------------
 
-# Python base code
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "__init__.py"), "{}/{}".format(PLG_DIRNAME, "__init__.py")
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "isogeo.py"), "{}/{}".format(PLG_DIRNAME, "isogeo.py")
-)
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "LICENSE"), Path(PLG_DIRNAME, "LICENSE"))
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "metadata.txt"), Path(PLG_DIRNAME, "metadata.txt"))
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "README.md"), Path(PLG_DIRNAME, "README"))
 
-# Modules
-modules_paths = Path("./modules")
-for module_file in list(modules_paths.glob("**/*.py")):
-    module_file_zip_path = PLG_DIRNAME / module_file.parent / module_file.name
-    RELEASE_ZIP.write(module_file.resolve(), module_file_zip_path.resolve())
+# -- PLUGIN PYTHON CODE ------------------------------------------------------------
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "__init__.py"), Path(PLG_DIRNAME, "__init__.py"))
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "isogeo.py"), Path(PLG_DIRNAME, "isogeo.py"))
 
-# Resources (media files)
-RESOURCES_FILES = [
-    path.relpath(f)
-    for f in listdir("resources")
-    if path.isfile(path.join(path.realpath("resources"), f))
-]
+# Python modules
+modules_path = Path("./modules")
+for file_src in list(modules_path.glob("**/*.py")):
+    file_dest_zip_path = PLG_DIRNAME / file_src.parent / file_src.name
+    RELEASE_ZIP.write(file_src, file_dest_zip_path)
 
-for resource in RESOURCES_FILES:
-    RELEASE_ZIP.write(
-        path.join(BASE_DIR_ABS, "resources", resource),
-        "{}/{}/{}".format(PLG_DIRNAME, "resources", resource),
-    )
+# UI files
+ui_path = Path("./ui")
+for file_src in list(ui_path.glob("**/*.py")):
+    if file_src.name.startswith("ui_"):
+        continue
+    file_dest_zip_path = PLG_DIRNAME / file_src.parent / file_src.name
+    RELEASE_ZIP.write(file_src, file_dest_zip_path)
+
+for file_src in list(ui_path.glob("**/*.ui")):
+    file_dest_zip_path = PLG_DIRNAME / file_src.parent / file_src.name
+    RELEASE_ZIP.write(file_src, file_dest_zip_path)
 
 # Translations
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "i18n", "isogeo_search_engine_fr.qm"),
-    "{}/{}/{}".format(PLG_DIRNAME, "i18n", "isogeo_search_engine_fr.qm"),
-)
+i18n_path = Path("./i18n")
+for file_src in list(i18n_path.glob("**/*.qm")):
+    file_dest_zip_path = PLG_DIRNAME / file_src.parent / file_src.name
+    RELEASE_ZIP.write(file_src, file_dest_zip_path)
+
+# Resources (media files)
+resources_path = Path("./resources")
+for resource_type in RESOURCES_TYPES:
+    for file_src in list(resources_path.glob(resource_type)):
+        file_dest_zip_path = PLG_DIRNAME / file_src.parent / file_src.name
+        RELEASE_ZIP.write(file_src, file_dest_zip_path)
 
 # UI - Base
+RELEASE_ZIP.write(Path(BASE_DIR_ABS, "icon.png"), Path(PLG_DIRNAME, "icon.png"))
 RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "icon.png"), "{}/{}".format(PLG_DIRNAME, "icon.png")
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "resources_rc.py"), "{}/{}".format(PLG_DIRNAME, "resources_rc.py")
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "resources.qrc"),
-    "{}/{}".format(PLG_DIRNAME, "resources.qrc"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "__init__.py"),
-    "{}/{}/{}".format(PLG_DIRNAME, "ui", "__init__.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "isogeo_dockwidget_base.ui"),
-    "{}/{}/{}".format(PLG_DIRNAME, "ui", "isogeo_dockwidget_base.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "isogeo_dockwidget.py"),
-    "{}/{}/{}".format(PLG_DIRNAME, "ui", "isogeo_dockwidget.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "ui_isogeo.py"),
-    "{}/{}/{}".format(PLG_DIRNAME, "ui", "ui_isogeo.py"),
-)
-
-# UI - Auth
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "auth", "__init__.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "auth", "__init__.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "auth", "ui_authentication.ui"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "auth", "ui_authentication.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "auth", "ui_authentication.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "auth", "ui_authentication.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "auth", "dlg_authentication.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "auth", "dlg_authentication.py"),
-)
-
-# UI - Credits
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "credits", "__init__.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "credits", "__init__.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "credits", "ui_credits.ui"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "credits", "ui_credits.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "credits", "ui_credits.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "credits", "ui_credits.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "credits", "dlg_credits.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "credits", "dlg_credits.py"),
-)
-
-# UI - MdDetails
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "metadata", "__init__.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "metadata", "__init__.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "metadata", "ui_md_details.ui"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "metadata", "ui_md_details.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "metadata", "ui_md_details.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "metadata", "ui_md_details.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "metadata", "dlg_md_details.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "metadata", "dlg_md_details.py"),
-)
-
-# UI - Quicksearch - name
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "__init__.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "__init__.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "ui_quicksearch_new.ui"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "ui_quicksearch_new.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "ui_quicksearch_new.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "ui_quicksearch_new.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "dlg_quicksearch_new.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "dlg_quicksearch_new.py"),
-)
-
-# UI - Quicksearch - rename
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "ui_quicksearch_rename.ui"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "ui_quicksearch_rename.ui"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "ui_quicksearch_rename.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "ui_quicksearch_rename.py"),
-)
-RELEASE_ZIP.write(
-    path.join(BASE_DIR_ABS, "ui", "quicksearch", "dlg_quicksearch_rename.py"),
-    "{}/{}/{}/{}".format(PLG_DIRNAME, "ui", "quicksearch", "dlg_quicksearch_rename.py"),
+    Path(BASE_DIR_ABS, "resources_rc.py"), Path(PLG_DIRNAME, "resources_rc.py")
 )
 
 # -- User settings ----------------------------------------------------------
