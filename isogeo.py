836f41527204416ee49d68447812a35fd722f947
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2020-12-17 12:23:25 +0100

    Clean code from dev logs
    #334

--- isogeo.py
+++ isogeo.py
@@ -347,18 +347,8 @@ class Isogeo:
     def onClosePlugin(self):
         """Cleanup necessary items here when plugin dockwidget is closed."""
         # save base portal URL in qsettings
-        logger.debug(
-            "*=====* DEBUG URL --> Isogeo : portal base URL value before plugin closing = {}".format(
-                self.form_mng.input_portal_url.text()
-            )
-        )
         qsettings.setValue(
             "isogeo/settings/portal_base_url", self.form_mng.input_portal_url.text()
-        )
-        logger.debug(
-            "*=====* DEBUG URL --> Isogeo : 'isogeo/settings/portal_base_url' setting = {}".format(
-                qsettings.value("isogeo/settings/portal_base_url")
-            )
         )
         # save cache
         self.form_mng.results_mng.cache_mng.dumper()
@@ -890,18 +880,8 @@ class Isogeo:
         self.form_mng.btn_credits.pressed.connect(self.credits_dialog.show)
 
         # -- Settings tab - Isogeo Portal settings ------------------------
-        logger.debug(
-            "*=====* DEBUG URL --> Isogeo : 'isogeo/settings/portal_base_url' setting = {}".format(
-                qsettings.value("isogeo/settings/portal_base_url")
-            )
-        )
         self.form_mng.input_portal_url.setText(
             qsettings.value("isogeo/settings/portal_base_url")
-        )
-        logger.debug(
-            "*=====* DEBUG URL --> Isogeo : input_portal_url.text() = {}".format(
-                self.form_mng.input_portal_url.text()
-            )
         )
 
         """ ------- EXECUTED AFTER PLUGIN IS LAUNCHED --------------------- """
