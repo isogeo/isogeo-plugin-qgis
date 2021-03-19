b0f45a28646e40084cfb492499837726272cb837
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2021-03-19 17:55:54 +0100

    Add a button to UI for pgdb conn reloading
    #320

--- ui/db_connections/ui_db_connections.py
+++ ui/db_connections/ui_db_connections.py
@@ -8,6 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_dlg_db_connections(object):
     def setupUi(self, dlg_db_connections):
         dlg_db_connections.setObjectName("dlg_db_connections")
@@ -15,10 +16,16 @@ class Ui_dlg_db_connections(object):
         dlg_db_connections.setMinimumSize(QtCore.QSize(490, 200))
         dlg_db_connections.setMaximumSize(QtCore.QSize(500, 300))
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/plugins/Isogeo/resources/settings/gear.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(
+            QtGui.QPixmap(":/plugins/Isogeo/resources/settings/gear.svg"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.Off,
+        )
         dlg_db_connections.setWindowIcon(icon)
         dlg_db_connections.setWindowOpacity(0.95)
-        dlg_db_connections.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        dlg_db_connections.setLocale(
+            QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates)
+        )
         dlg_db_connections.setSizeGripEnabled(False)
         self.gridLayout = QtWidgets.QGridLayout(dlg_db_connections)
         self.gridLayout.setObjectName("gridLayout")
@@ -28,7 +35,9 @@ class Ui_dlg_db_connections(object):
         self.verticalLayout.setSpacing(12)
         self.verticalLayout.setObjectName("verticalLayout")
         self.label = QtWidgets.QLabel(dlg_db_connections)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
@@ -43,13 +52,15 @@ class Ui_dlg_db_connections(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.tbl = QtWidgets.QTableWidget(dlg_db_connections)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tbl.sizePolicy().hasHeightForWidth())
         self.tbl.setSizePolicy(sizePolicy)
-        self.tbl.setMinimumSize(QtCore.QSize(0, 0))
-        self.tbl.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.tbl.setMinimumSize(QtCore.QSize(0, 100))
+        self.tbl.setMaximumSize(QtCore.QSize(16777215, 200))
         self.tbl.setAutoFillBackground(False)
         self.tbl.setEditTriggers(QtWidgets.QAbstractItemView.CurrentChanged)
         self.tbl.setAlternatingRowColors(False)
@@ -67,13 +78,35 @@ class Ui_dlg_db_connections(object):
         self.tbl.verticalHeader().setVisible(False)
         self.horizontalLayout.addWidget(self.tbl)
         self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum
+        )
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.btn_reload_conn = QtWidgets.QPushButton(dlg_db_connections)
+        self.btn_reload_conn.setMinimumSize(QtCore.QSize(200, 0))
+        self.btn_reload_conn.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.btn_reload_conn.setObjectName("btn_reload_conn")
+        self.horizontalLayout_2.addWidget(self.btn_reload_conn)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        spacerItem1 = QtWidgets.QSpacerItem(
+            20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding
+        )
+        self.verticalLayout.addItem(spacerItem1)
         self.btnbox = QtWidgets.QDialogButtonBox(dlg_db_connections)
         self.btnbox.setMaximumSize(QtCore.QSize(600, 16777215))
         self.btnbox.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.btnbox.setAutoFillBackground(False)
-        self.btnbox.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        self.btnbox.setLocale(
+            QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates)
+        )
         self.btnbox.setOrientation(QtCore.Qt.Horizontal)
-        self.btnbox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Reset|QtWidgets.QDialogButtonBox.Save)
+        self.btnbox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.Cancel
+            | QtWidgets.QDialogButtonBox.Reset
+            | QtWidgets.QDialogButtonBox.Save
+        )
         self.btnbox.setCenterButtons(True)
         self.btnbox.setObjectName("btnbox")
         self.verticalLayout.addWidget(self.btnbox)
@@ -86,17 +119,27 @@ class Ui_dlg_db_connections(object):
 
     def retranslateUi(self, dlg_db_connections):
         _translate = QtCore.QCoreApplication.translate
-        dlg_db_connections.setWindowTitle(_translate("dlg_db_connections", "PostGIS database configuration"))
-        self.label.setText(_translate("dlg_db_connections", "Choose the embed connection to be used to access to each PostGIS database"))
+        dlg_db_connections.setWindowTitle(
+            _translate("dlg_db_connections", "PostGIS database configuration")
+        )
+        self.label.setText(
+            _translate(
+                "dlg_db_connections",
+                "Choose the embed connection to be used to access to each PostGIS database",
+            )
+        )
         self.tbl.setSortingEnabled(False)
+        self.btn_reload_conn.setText(
+            _translate("dlg_db_connections", "Reload embed connection(s)")
+        )
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     dlg_db_connections = QtWidgets.QDialog()
     ui = Ui_dlg_db_connections()
     ui.setupUi(dlg_db_connections)
     dlg_db_connections.show()
     sys.exit(app.exec_())
-
