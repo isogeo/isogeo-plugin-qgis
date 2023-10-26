2d75622e9f49fc6ac7c5023948012c3b12c3ec62
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2023-10-26 17:10:36 +0200

    #437 Keep investigating

--- tests/dev/qgis_console/current_extent_polygone.py
+++ tests/dev/qgis_console/current_extent_polygone.py
@@ -1,19 +1,45 @@
-from qgis.core import Qgis, QgsRectangle, QgsGeometry, QgsPointXY, QgsFeature, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject, QgsRenderContext, QgsRasterLayer, QgsVectorLayer
+from qgis.core import Qgis, QgsGeometry, QgsPointXY, QgsFeature, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject, QgsRenderContext, QgsVectorLayer
 from qgis.utils import iface
 from qgis.PyQt.QtGui import QColor
 
 current_crs = iface.mapCanvas().mapSettings().destinationCrs()
+
+current_crs_bound_wgs84 = current_crs.bounds()
 wgs84_crs = QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.EpsgCrsId)
 
 CRS_converter = QgsCoordinateTransform(current_crs, wgs84_crs, QgsProject.instance())
 
-
+current_crs_bound = CRS_converter.transformBoundingBox(current_crs_bound_wgs84, Qgis.TransformDirection(1))
 currentCRS_rectangle = iface.mapCanvas().extent()
-coord_currentCRS = "{},{},{},{}".format(currentCRS_rectangle.xMinimum(), currentCRS_rectangle.yMinimum(), currentCRS_rectangle.xMaximum(), currentCRS_rectangle.yMaximum())
+
+if current_crs_bound.contains(currentCRS_rectangle):
+    print("inbound")
+    pass
+else:
+    print("outbound")
+    if currentCRS_rectangle.xMinimum() < current_crs_bound.xMinimum():
+        currentCRS_rectangle.setXMinimum(current_crs_bound.xMinimum())
+    else:
+        pass
+
+    if currentCRS_rectangle.yMinimum() < current_crs_bound.yMinimum():
+        currentCRS_rectangle.setYMinimum(current_crs_bound.yMinimum())
+    else:
+        pass
+
+    if currentCRS_rectangle.xMaximum() > current_crs_bound.xMaximum():
+        currentCRS_rectangle.setXMaximum(current_crs_bound.xMaximum())
+    else:
+        pass
+
+    if currentCRS_rectangle.yMaximum() > current_crs_bound.yMaximum():
+        currentCRS_rectangle.setYMaximum(current_crs_bound.yMaximum())
+    else:
+        pass
 
 wgs84_rectangle = CRS_converter.transformBoundingBox(currentCRS_rectangle)
-coord_wgs84 = "{},{},{},{}".format(wgs84_rectangle.xMinimum(), wgs84_rectangle.yMinimum(), wgs84_rectangle.xMaximum(), wgs84_rectangle.yMaximum())
 
+currentCRS_rectangle = iface.mapCanvas().extent()
 points_currentCRS = [
     QgsPointXY(currentCRS_rectangle.xMinimum(), currentCRS_rectangle.yMaximum()),
     QgsPointXY(currentCRS_rectangle.xMaximum(), currentCRS_rectangle.yMaximum()),
@@ -26,11 +52,15 @@ bbox_polygon_currentCRS.setGeometry(bbox_geometry_currentCRS)
 bbox_layer_currentCRS = QgsVectorLayer("Polygon?crs=epsg:{}".format(int(str(current_crs.authid()).split(":")[1])), "BoundingBox_currentCRS", "memory")
 bbox_layer_currentCRS.dataProvider().addFeatures([bbox_polygon_currentCRS])
 
+wgs84_xMin = wgs84_rectangle.xMinimum()
+wgs84_yMin = wgs84_rectangle.yMinimum()
+wgs84_xMax = wgs84_rectangle.xMaximum()
+wgs84_yMax = wgs84_rectangle.yMaximum()
 points_wgs84 = [
-    QgsPointXY(wgs84_rectangle.xMinimum(), wgs84_rectangle.yMaximum()),
-    QgsPointXY(wgs84_rectangle.xMaximum(), wgs84_rectangle.yMaximum()),
-    QgsPointXY(wgs84_rectangle.xMaximum(), wgs84_rectangle.yMinimum()),
-    QgsPointXY(wgs84_rectangle.xMinimum(), wgs84_rectangle.yMinimum())
+    QgsPointXY(wgs84_xMin, wgs84_yMax),
+    QgsPointXY(wgs84_xMax, wgs84_yMax),
+    QgsPointXY(wgs84_xMax, wgs84_yMin),
+    QgsPointXY(wgs84_xMin, wgs84_yMin)
 ]
 bbox_geometry_wgs84 = QgsGeometry.fromPolygonXY([points_wgs84])
 bbox_polygon_wgs84 = QgsFeature()
