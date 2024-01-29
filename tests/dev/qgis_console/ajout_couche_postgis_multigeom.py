66655158281e57915ec684798d504d2f72fe684d
SimonSAMPERE <41120579+SimonSAMPERE@users.noreply.github.com>
2024-01-29 16:26:54 +0100

    Update standalone script to test multi geom PostGIS tables adding

--- tests/dev/qgis_console/ajout_couche_postgis_multigeom.py
+++ tests/dev/qgis_console/ajout_couche_postgis_multigeom.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from qgis.core import QgsDataSourceUri, QgsProject
+from qgis.core import QgsDataSourceUri, QgsProject, QgsVectorLayer, QgsCoordinateReferenceSystem
 from db_manager.db_plugins.postgis.connector import PostGisDBConnector
 from db_manager.db_plugins.postgis.plugin import PostGisDBPlugin
 from pprint import pprint
@@ -15,11 +15,26 @@ port=env.get("ajout_couche_postgis_multigeom").get("port")
 db_name=env.get("ajout_couche_postgis_multigeom").get("db_name")
 user=env.get("ajout_couche_postgis_multigeom").get("user")
 password=env.get("ajout_couche_postgis_multigeom").get("password")
+table_name = "multilines_2154_specchar_attributes"
+table_name = "multigeom_2154"
 
 uri.setConnection(aHost=host, aPort=port, aDatabase=db_name, aUsername=user, aPassword=password)
 
-pgis_db_plg = PostGisDBPlugin("AlwaysData_isogeoReader")
+pgis_db_plg = PostGisDBPlugin("isogeo_test")
 c = PostGisDBConnector(uri, pgis_db_plg)
+
+for tab in c.getTables():
+    if tab[2] == "sample" and tab[1] == table_name:
+        print(tab)
+
+uri.setWkbType(3)
+uri.setDataSource("sample", table_name, "geom","","gid")
+vlayer = QgsVectorLayer(uri.uri(), table_name, 'postgres')
+uri.setSrid("2154")
+vlayer.setCrs(QgsCoordinateReferenceSystem('EPSG:2154'))
+
+print(vlayer.wkbType())
+QgsProject.instance().addMapLayer(vlayer)
 
 #tables = c.getTables("test")
 #pprint(tables)
