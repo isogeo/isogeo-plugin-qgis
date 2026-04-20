4d8239d3bef48d27f6f1f81987572603013bd578
SimonSAMPERE <simon.sampere@isogeo.fr>
2026-04-20 15:39:19 +0200

    Drop pyuic5 from build scripts and automate resources_rc.py import fix

--- make.bat
+++ make.bat
@@ -6,12 +6,10 @@ call qt5-tools lrelease .\i18n\isogeo_search_engine_es.ts
 call qt5-tools lrelease .\i18n\isogeo_search_engine_pt_BR.ts
 
 Echo "Updating Qt resources files (images)"
-REM NOTE: after pyrcc5, manually change line 9 of resources_rc.py:
-REM   from PyQt5 import QtCore  ->  from qgis.PyQt import QtCore
 call pyrcc5 resources.qrc -o resources_rc.py
+powershell -Command "(Get-Content resources_rc.py) -replace 'from PyQt5 import QtCore', 'from qgis.PyQt import QtCore' | Set-Content resources_rc.py"
 
 Echo "Packaging plugin"
 call python tools\plugin_packager.py
 
 Echo "End of compilation."
-Echo "Remind that you have to remove 'import resources_rc' at the end of py files generated from ui."
